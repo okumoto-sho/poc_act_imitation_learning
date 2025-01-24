cf596017ebf0e3ec9f11e6aa5dd76de3fd4b1f8d
Sho Okumoto <lebesgue0118@gmail.com>
2025-01-25 03:58:58 +0900

    add move_q example

---
+++ examples/robot_client/move_q.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from koch11.dynamixel.koch11 import make_client
+
+client = make_client()
+client.make_control_enable()
+
+q = np.array([0, 0, 0, 0, 0])
+client.move_q(q)
+
+# client.make_control_disable()
