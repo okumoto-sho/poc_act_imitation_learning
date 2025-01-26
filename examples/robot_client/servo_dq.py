07a6defb029f9f0b4f8aa92b47d877a0e5e34e79
Sho Okumoto <lebesgue0118@gmail.com>
2025-01-26 17:50:19 +0900

    Add example for servo_dq

--- examples/robot_client/servo_dq.py
+++ examples/robot_client/servo_dq.py
@@ -12,14 +12,14 @@ client.make_control_enable()
 present_q = client.get_present_q()
 client.move_q(np.array([0.0, 0.0, 0.0, 0.0, 0.0]))
 start = time.time()
-client.servo_dq(np.array([1.0, 0.0, 0.0, 0.0, 0.0]))
+client.servo_dq(np.array([2.0, 0.0, 0.0, 0.0, 0.0]))
 while time.time() - start < 10:
     prev_q = present_q
     present_q = client.get_present_q()
     if present_q[0] >= np.pi / 2 and prev_q[0] < np.pi / 2:
-        client.servo_dq(np.array([-1.0, 0.0, 0.0, 0.0, 0.0]))
+        client.servo_dq(np.array([-2.0, 0.0, 0.0, 0.0, 0.0]))
     elif present_q[0] <= -np.pi / 2 and prev_q[0] > -np.pi / 2:
-        client.servo_dq(np.array([1.0, 0.0, 0.0, 0.0, 0.0]))
+        client.servo_dq(np.array([2.0, 0.0, 0.0, 0.0, 0.0]))
 
 client.make_control_disable()
 client.set_operation_mode(OperatingMode.PositionControl)
