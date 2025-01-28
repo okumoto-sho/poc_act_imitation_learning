8de1ec10646ed68e48f144874109f7550a45cf8a
Sho Okumoto <lebesgue0118@gmail.com>
2025-01-29 00:20:14 +0900

    Add the conversion from unsigned int32 to signed one. Q

--- koch11/dynamixel/dynamixel_client.py
+++ koch11/dynamixel/dynamixel_client.py
@@ -14,62 +14,78 @@ from enum import Enum
 from typing import List
 
 ControlTableElementDescription = namedtuple(
-    "ControlTableElement", ["address", "num_bytes", "is_read_only"]
+    "ControlTableElement", ["address", "num_bytes", "is_read_only", "signed"]
 )
 
 
 class ControlTable(Enum):
-    ModelNumber = ControlTableElementDescription(0, 2, True)
-    ModelInformation = ControlTableElementDescription(2, 4, True)
-    FirmwareVersion = ControlTableElementDescription(6, 1, True)
-    Id = ControlTableElementDescription(7, 1, False)
-    BaudRate = ControlTableElementDescription(8, 1, False)
-    ReturnDelayTime = ControlTableElementDescription(9, 1, False)
-    DriveMode = ControlTableElementDescription(10, 1, False)
-    OperatingMode = ControlTableElementDescription(11, 1, False)
-    SecondaryId = ControlTableElementDescription(12, 1, False)
-    ProtocolType = ControlTableElementDescription(13, 1, False)
-    HomingOffset = ControlTableElementDescription(20, 4, False)
-    MovingThreshold = ControlTableElementDescription(24, 4, False)
-    TemperatureLimit = ControlTableElementDescription(31, 1, False)
-    MaxVoltageLimit = ControlTableElementDescription(32, 2, False)
-    MinVoltageLimit = ControlTableElementDescription(34, 2, False)
-    PwmLimit = ControlTableElementDescription(36, 2, False)
-    VelocityLimit = ControlTableElementDescription(44, 4, False)
-    MaxPositionLimit = ControlTableElementDescription(48, 4, False)
-    MinPositionLimit = ControlTableElementDescription(52, 4, False)
-    StartupConfiguration = ControlTableElementDescription(60, 1, False)
-    Shutdown = ControlTableElementDescription(63, 1, False)
-    TorqueEnable = ControlTableElementDescription(64, 1, False)
-    Led = ControlTableElementDescription(65, 1, False)
-    StatusReturnLevel = ControlTableElementDescription(68, 1, False)
-    RegisteredInstruction = ControlTableElementDescription(69, 1, True)
-    HardwareErrorStatus = ControlTableElementDescription(70, 1, True)
-    VelocityIGain = ControlTableElementDescription(76, 2, False)
-    VelocityPGain = ControlTableElementDescription(78, 2, False)
-    PositionDGain = ControlTableElementDescription(80, 2, False)
-    PositionIGain = ControlTableElementDescription(82, 2, False)
-    PositionPGain = ControlTableElementDescription(84, 2, False)
-    FeedForward2ndGain = ControlTableElementDescription(88, 2, False)
-    FeedForward1stGain = ControlTableElementDescription(90, 2, False)
-    BusWatchdog = ControlTableElementDescription(98, 1, False)
-    GoalPwm = ControlTableElementDescription(100, 2, False)
-    GoalVelocity = ControlTableElementDescription(104, 4, False)
-    ProfileAcceleration = ControlTableElementDescription(108, 4, False)
-    ProfileVelocity = ControlTableElementDescription(112, 4, False)
-    GoalPosition = ControlTableElementDescription(116, 4, False)
-    RealtimeTick = ControlTableElementDescription(120, 2, True)
-    Moving = ControlTableElementDescription(122, 1, True)
-    MovingStatus = ControlTableElementDescription(123, 1, True)
-    PresentPwm = ControlTableElementDescription(124, 2, True)
-    PresentLoad = ControlTableElementDescription(126, 2, True)
-    PresentVelocity = ControlTableElementDescription(128, 4, True)
-    PresentPosition = ControlTableElementDescription(132, 4, True)
-    VelocityTrajectory = ControlTableElementDescription(136, 4, True)
-    PositionTrajectory = ControlTableElementDescription(140, 4, True)
-    PresentInputVoltage = ControlTableElementDescription(144, 2, True)
-    PresentTemperature = ControlTableElementDescription(146, 1, True)
-    BackupReady = ControlTableElementDescription(147, 1, True)
+    ModelNumber = ControlTableElementDescription(0, 2, True, False)
+    ModelInformation = ControlTableElementDescription(2, 4, True, False)
+    FirmwareVersion = ControlTableElementDescription(6, 1, True, False)
+    Id = ControlTableElementDescription(7, 1, False, False)
+    BaudRate = ControlTableElementDescription(8, 1, False, False)
+    ReturnDelayTime = ControlTableElementDescription(9, 1, False, False)
+    DriveMode = ControlTableElementDescription(10, 1, False, False)
+    OperatingMode = ControlTableElementDescription(11, 1, False, False)
+    SecondaryId = ControlTableElementDescription(12, 1, False, False)
+    ProtocolType = ControlTableElementDescription(13, 1, False, False)
+    HomingOffset = ControlTableElementDescription(20, 4, False, True)
+    MovingThreshold = ControlTableElementDescription(24, 4, False, False)
+    TemperatureLimit = ControlTableElementDescription(31, 1, False, False)
+    MaxVoltageLimit = ControlTableElementDescription(32, 2, False, False)
+    MinVoltageLimit = ControlTableElementDescription(34, 2, False, False)
+    PwmLimit = ControlTableElementDescription(36, 2, False, False)
+    VelocityLimit = ControlTableElementDescription(44, 4, False, False)
+    MaxPositionLimit = ControlTableElementDescription(48, 4, False, False)
+    MinPositionLimit = ControlTableElementDescription(52, 4, False, False)
+    StartupConfiguration = ControlTableElementDescription(60, 1, False, False)
+    Shutdown = ControlTableElementDescription(63, 1, False, False)
+    TorqueEnable = ControlTableElementDescription(64, 1, False, False)
+    Led = ControlTableElementDescription(65, 1, False, False)
+    StatusReturnLevel = ControlTableElementDescription(68, 1, False, False)
+    RegisteredInstruction = ControlTableElementDescription(69, 1, True, False)
+    HardwareErrorStatus = ControlTableElementDescription(70, 1, True, False)
+    VelocityIGain = ControlTableElementDescription(76, 2, False, False)
+    VelocityPGain = ControlTableElementDescription(78, 2, False, False)
+    PositionDGain = ControlTableElementDescription(80, 2, False, False)
+    PositionIGain = ControlTableElementDescription(82, 2, False, False)
+    PositionPGain = ControlTableElementDescription(84, 2, False, False)
+    FeedForward2ndGain = ControlTableElementDescription(88, 2, False, False)
+    FeedForward1stGain = ControlTableElementDescription(90, 2, False, False)
+    BusWatchdog = ControlTableElementDescription(98, 1, False, False)
+    GoalPwm = ControlTableElementDescription(100, 2, False, True)
+    GoalVelocity = ControlTableElementDescription(104, 4, False, True)
+    ProfileAcceleration = ControlTableElementDescription(108, 4, False, False)
+    ProfileVelocity = ControlTableElementDescription(112, 4, False, False)
+    GoalPosition = ControlTableElementDescription(116, 4, False, True)
+    RealtimeTick = ControlTableElementDescription(120, 2, True, False)
+    Moving = ControlTableElementDescription(122, 1, True, False)
+    MovingStatus = ControlTableElementDescription(123, 1, True, False)
+    PresentPwm = ControlTableElementDescription(124, 2, True, True)
+    PresentLoad = ControlTableElementDescription(126, 2, True, True)
+    PresentVelocity = ControlTableElementDescription(128, 4, True, True)
+    PresentPosition = ControlTableElementDescription(132, 4, True, True)
+    VelocityTrajectory = ControlTableElementDescription(136, 4, True, True)
+    PositionTrajectory = ControlTableElementDescription(
+        140,
+        4,
+        True,
+        True,
+    )
+    PresentInputVoltage = ControlTableElementDescription(144, 2, True, True)
+    PresentTemperature = ControlTableElementDescription(146, 1, True, False)
+    BackupReady = ControlTableElementDescription(147, 1, True, False)
+
+
+def _to_signed_int(value: int, num_bytes: int):
+    if num_bytes == 1:
+        return -(0x100 - value) if ((value & 0x80) >> 7) != 0 else value
+    if num_bytes == 2:
+        return -(0x10000 - value) if ((value & 0x8000) >> 15) != 0 else value
+    if num_bytes == 4:
+        return -(0x100000000 - value) if ((value & 0x80000000) >> 31) != 0 else value
+    else:
+        raise ValueError("Invalid num_bytes are detected")
 
 
 class DynamixelXLSeriesClient:
@@ -173,6 +189,9 @@ class DynamixelXLSeriesClient:
                 f"Read operation failed because of {self.packet_handler.getRxPacketError(error)}"
             )
 
+        if control_table.value.signed:
+            _to_signed_int(value, control_table.value.num_bytes)
+
         return value
 
     def sync_read(
@@ -196,12 +215,19 @@ class DynamixelXLSeriesClient:
                 f"Communication failed because of {self.packet_handler.getTxRxResult(comm_result)}"
             )
 
-        return [
+        raw_value = [
             self.group_sync_read[control_table].getData(
                 motor_id, control_table.value.address, control_table.value.num_bytes
             )
             for motor_id in motor_ids
         ]
+
+        if control_table.value.signed:
+            return [
+                _to_signed_int(val, control_table.value.num_bytes) for val in raw_value
+            ]
+
+        return raw_value
 
     def sync_write(
         self,
