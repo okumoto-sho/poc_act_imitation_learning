607189c40e57a2b553f3f4048bea3341b98d20b3
Sho Okumoto <lebesgue0118@gmail.com>
2025-02-02 21:13:03 +0900

    Change the speed of move_q in teleoperation example

--- examples/robot_client/teleoperation.py
+++ examples/robot_client/teleoperation.py
@@ -1,15 +1,23 @@
 import numpy as np
+import time
 
 from koch11.dynamixel.koch11 import make_leader_client, make_follower_client
 
 
 follower = make_follower_client()
 follower.make_control_enable()
-follower.move_q(np.array([0, 0, 0, 0, 0, 0]))
+follower.move_q(np.array([0, 0, 0, 0, 0, 0]), speed=2.0)
 
 leader = make_leader_client()
 
 while True:
+    start = time.time()
+    q_pos = follower.get_present_q()
     q = leader.get_present_q()
     follower.servo_q(q)
-    print(q)
+    end = time.time()
+    print(
+        q_pos,
+        q,
+        f"FPS: {1 / (end - start)}",
+    )
