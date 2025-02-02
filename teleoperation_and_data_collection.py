4ba1cab09ee2cd0f7fe8737aa3ff6fefd66b7bf8
Sho Okumoto <lebesgue0118@gmail.com>
2025-02-02 22:08:46 +0900

    Make teloperator enable to give the number of teloperation in the arguments of teleoperation_and_data_collection.py

--- teleoperation_and_data_collection.py
+++ teleoperation_and_data_collection.py
@@ -2,9 +2,78 @@ import argparse
 import cv2 as cv
 import time
 import h5py
+import numpy as np
 
 from koch11.dynamixel.koch11 import make_leader_client, make_follower_client
 from teleoperation_config import teleoperation_config
+
+
+def execute_single_teleoperation_step(
+    episode_id: int, task_name: str, dataset_dir: str, follower, leader, cameras
+):
+    # wait for press 'q' to start the data collection
+    control_cycle = teleoperation_config["control_cycle"]
+    print("Start the teleoperation.")
+    print("Press 'q' to start the data collection.")
+    while cv.waitKey(1) != ord("q"):
+        start = time.time()
+        command_q = leader.get_present_q()
+        follower.servo_q(command_q)
+        for cap in cameras:
+            ret, frame = cap.read()
+            frame = cv.flip(frame, 0)
+            frame = cv.flip(frame, 1)
+            cv.imshow("Frame", frame)
+        end = time.time()
+        if (end - start) < control_cycle:
+            time.sleep(control_cycle - (end - start))
+
+    # prepare the data dictionary
+    max_time_steps = teleoperation_config["episode_len_time_steps"]
+    data_dict = {
+        "/observations/qpos": np.zeros(shape=(max_time_steps, 6)),
+        "/action": np.zeros(shape=(max_time_steps, 6)),
+    }
+    for cam in teleoperation_config["camera_configs"]:
+        data_dict[f"/observations/images/{cam['device_id']}"] = np.zeros(
+            shape=(max_time_steps, cam["height"], cam["width"], 3)
+        )
+
+    # start the data collection
+    print("Start data collection.")
+    for step in range(max_time_steps):
+        start = time.time()
+        qpos = follower.get_present_q()
+        command_q = leader.get_present_q()
+
+        data_dict["/observations/qpos"][step, :] = qpos
+        data_dict["/action"][step, :] = command_q
+        for i, cap in enumerate(cameras):
+            ret, frame = cap.read()
+            frame = cv.flip(frame, 0)
+            frame = cv.flip(frame, 1)
+            data_dict[f"/observations/images/{i}"][step, :] = frame
+
+        follower.servo_q(command_q)
+        end = time.time()
+        if (end - start) < control_cycle:
+            time.sleep(control_cycle - (end - start))
+        print(f"Coillecting data. Step: {step}, FPS: {1 / (end - start)}")
+
+    with h5py.File(f"{args.dataset_dir}/{args.task_name}_{episode_id}.h5", "w") as f:
+        obs = f.create_group("observations")
+        images = obs.create_group("images")
+        for cam in teleoperation_config["camera_configs"]:
+            images.create_dataset(
+                f"{cam['device_id']}",
+                (max_time_steps, cam["height"], cam["width"], 3),
+                dtype="uint8",
+            )
+        obs.create_dataset("qpos", (max_time_steps, 6), dtype="float32")
+        f.create_dataset("action", (max_time_steps, 6), dtype="float32")
+
+        for name, array in data_dict.items():
+            f[name][...] = array
 
 
 def main(args):
@@ -24,74 +93,20 @@ def main(args):
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, camera_config["height"])
         cameras.append(cap)
 
-    # get the follower robot move to initial position
-    follower.move_q(teleoperation_config["follower_initail_q"], speed=1.0)
-
-    # wait for press 'q' to start the data collection
-    control_cycle = teleoperation_config["control_cycle"]
-    print("Start the teleoperation.")
-    print("Press 'q' to start the data collection.")
-    while cv.waitKey(1) != ord("q"):
-        start = time.time()
-        leader_q = leader.get_present_q()
-        follower.servo_q(leader_q)
-        for cap in cameras:
-            ret, frame = cap.read()
-            frame = cv.flip(frame, 0)
-            frame = cv.flip(frame, 1)
-            cv.imshow("Frame", frame)
-        end = time.time()
-        if (end - start) < control_cycle:
-            time.sleep(control_cycle - (end - start))
-
-    # prepare the data dictionary
-    data_dict = {"/observations/qpos": [], "/action": []}
-    for cam in teleoperation_config["camera_configs"]:
-        data_dict[f"/observations/images/{cam['device_id']}"] = []
-
-    # start the data collection
-    print("Start data collection.")
-    max_time_steps = teleoperation_config["episode_len_time_steps"]
-    for step in range(max_time_steps):
-        start = time.time()
-        qpos = follower.get_present_q()
-        leader_q = leader.get_present_q()
-
-        data_dict["/observations/qpos"].append(qpos)
-        data_dict["/action"].append(leader_q)
-        for i, cap in enumerate(cameras):
-            ret, frame = cap.read()
-            frame = cv.flip(frame, 0)
-            frame = cv.flip(frame, 1)
-            data_dict[f"/observations/images/{i}"].append(frame)
-
-        follower.servo_q(leader_q)
-        end = time.time()
-        if (end - start) < control_cycle:
-            time.sleep(control_cycle - (end - start))
-
-    with h5py.File(
-        f"{args.dataset_dir}/{args.task_name}_{args.episode_id}.h5", "w"
-    ) as f:
-        obs = f.create_group("observations")
-        images = obs.create_group("images")
-        for cam in teleoperation_config["camera_configs"]:
-            images.create_dataset(
-                f"{cam['device_id']}",
-                (max_time_steps, cam["height"], cam["width"], 3),
-                dtype="uint8",
-            )
-        obs.create_dataset("qpos", (max_time_steps, 6), dtype="float32")
-        f.create_dataset("action", (max_time_steps, 6), dtype="float32")
-
-        for name, array in data_dict.items():
-            f[name][...] = array
+    # Repeat the teleoperation and data collection for the specified number of episodes
+    for episode_id in range(
+        args.initial_episode_id, args.initial_episode_id + args.num_episodes
+    ):
+        execute_single_teleoperation_step(
+            episode_id, args.task_name, args.dataset_dir, follower, leader, cameras
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--episode_id", type=int)
-    parser.add_argument("--dataset_dir", type=str)
-    parser.add_argument("--task_name", type=str)
+    parser.add_argument("--initial_episode_id", type=int, default=0)
+    parser.add_argument("--dataset_dir", type=str, default="dataset")
+    parser.add_argument("--task_name", type=str, default="teleoperation")
+    parser.add_argument("--num_episodes", type=int, default=50)
     args = parser.parse_args()
     main(args)
