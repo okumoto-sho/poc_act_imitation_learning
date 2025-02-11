0acd7c11d548c6009bb3f95f6737af3638847600
Sho Okumoto <lebesgue0118@gmail.com>
2025-02-11 16:55:01 +0900

    Add camera_names and camera_config_2 to teloperation_config.py

--- teleoperation_config.py
+++ teleoperation_config.py
@@ -1,18 +1,31 @@
 import numpy as np
 
-camera_config: dict = {
+camera_config_1: dict = {
     "fourcc": "MJPG",
     "fps": 120.0,
     "width": 640,
     "height": 480,
     "device_id": 0,
+    "device_name": "0",
+}
+camera_config_2: dict = {
+    "fourcc": "MJPG",
+    "fps": 120.0,
+    "width": 640,
+    "height": 480,
+    "device_id": 2,
+    "device_name": "2",
 }
 
 teleoperation_config: dict = {
-    "leader_device": "/dev/ttyACM1",
-    "follower_device": "/dev/ttyACM2",
-    "episode_len_time_steps": 1000,
-    "control_cycle": 0.01,
-    "camera_configs": [camera_config],
+    "leader_device": "/dev/ttyACM0",
+    "follower_device": "/dev/ttyACM1",
+    "episode_len_time_steps": 500,
+    "control_cycle": 0.025,
+    "camera_configs": [camera_config_1],
     "follower_initail_q": np.array([0, 0, 0, 0, 0, 0]),
 }
+
+camera_names = [
+    config["device_name"] for config in teleoperation_config["camera_configs"]
+]
