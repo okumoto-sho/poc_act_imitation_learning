582e0d3011a3ca60e0497ed7fe4cee3c4649259e
Sho Okumoto <lebesgue0118@gmail.com>
2025-02-11 16:50:52 +0900

    add model_config.py

---
+++ model_config.py
@@ -0,0 +1,13 @@
+model_config: dict = {
+    "emb_dim": 512,
+    "action_chunk_size": 100,
+    "image_feat_seq_length": 300,
+    "action_dim": 6,
+    "qpos_dim": 6,
+    "z_dim": 32,
+    "n_enc_layers": 4,
+    "n_dec_layers": 7,
+    "n_heads": 8,
+    "feedforward_dim": 3200,
+    "beta": 10,
+}
