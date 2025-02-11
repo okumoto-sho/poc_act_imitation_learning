45dab51bf6ae974aacdab908139a2033933320a9
Sho Okumoto <lebesgue0118@gmail.com>
2025-02-12 00:17:14 +0900

    Refactor

--- koch11/camera.py
+++ koch11/camera.py
@@ -5,18 +5,20 @@ class Camera:
     def __init__(
         self,
         device_id: str,
+        device_name: str,
         fps: int = 120,
         width: int = 640,
         height: int = 480,
         fourcc: str = "MJPG",
     ):
+        self.device_name_ = device_name
         self.device_id_ = device_id
         self.cap = cv.VideoCapture(int(device_id))
         self.cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*fourcc))
         self.cap.set(cv.CAP_PROP_FPS, fps)
         self.cap.set(cv.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, height)
-        self.cap.set(cv.CAP_PROP_BUFFERSIZE, 4.0)
+        self.cap.set(cv.CAP_PROP_BUFFERSIZE, 3)
 
     def read(self, flip: bool = True):
         ret, frame = self.cap.read()
@@ -29,7 +31,11 @@ class Camera:
 
     @property
     def device_id(self):
-        return self._device_id_
+        return self.device_id_
+
+    @property
+    def device_name(self):
+        return self.device_name_
 
     @property
     def width(self):
